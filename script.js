5be69cf11c438e9a9b518ab436fc8d81359da577
Daniel Somngesi <u19329033@tuks.co.za>
2022-01-28 14:20:26 +0200

    added pre-loader

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+jQuery(document).ready(function() {
+
+  jQuery(window).load(function() {
+      $(".loader").fadeOut(25000);
+  });
+
+});
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -8,3 +16,4 @@ function myFunction() {
   }
 var dt = new Date();
 document.getElementById('date-time').innerHTML = dt.getFullYear();
+
