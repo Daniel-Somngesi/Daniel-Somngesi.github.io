e56d92527de7f4eb9591efea2f8b60cd69268d2d
Daniel Somngesi <u19329033@tuks.co.za>
2022-01-31 23:52:36 +0200

    added preloader

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+var dt = new Date();
+document.getElementById('date-time').innerHTML = dt.getFullYear();
+
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
