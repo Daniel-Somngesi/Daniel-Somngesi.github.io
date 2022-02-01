5701966be5f94f50f0c17e87c087eeb8201b4987
Daniel Somngesi <u19329033@tuks.co.za>
2022-02-01 16:59:28 +0200

    added preloader for mobile

--- script.js
+++ script.js
@@ -12,8 +12,14 @@ var dt = new Date();
 document.getElementById('date-time').innerHTML = dt.getFullYear();
 
 var loader = document.getElementById("preloader");
+var loader1 = document.getElementById("preloader1");
+var loader2 = document.getElementById("preloader2");
+var loader3 = document.getElementById("preloader3");
 
 window.addEventListener("load", function(){
     loader.style.display = "none";
+    loader1.style.display = "none";
+    loader2.style.display = "none";
+    loader3.style.display = "none";
 })
 
